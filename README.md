0327f441b9629b63df76e18bcdb72486723befad
deimos <deimoshall.dev@proton.me>
2026-04-23 09:36:43 -0600

    refactor: remove tokio and error chain

--- README.md
+++ README.md
@@ -3,36 +3,39 @@
 A simple library to decode dlc files to a readable format.
 
 ## Usage
+
 Add `dlc_decrypter` as a dependency in `Cargo.toml`:
+
 ```toml
 [dependencies]
-dlc-decrypter = "0.2.1"
+dlc-decoder = "0.2.2"
 ```
 
-Use the `dlc_decrypter::DlcDecoder' to decrypt a .dlc file or datapackage:
+Use the `dlc_decoder::DlcDecoder' to decrypt a .dlc file or datapackage:
+
 ```rust
 extern crate dlc_decrypter;
 
+use dlc_decrypter::DlcDecoder;
+use std::env;
+
 fn main() {
-    // Create the DlcDecoder
-    let dd = dlc_decrypter::DlcDecoder::new();
+    let decoder = DlcDecoder::new();
 
-    // loop over all arguments for the programm
-    // skip the first one because it's the programm
-    // own name
-    for arg in std::env::args().skip(1) {
-        // hand over the file path
-        let dlc = dd.from_file(arg);
+    for arg in env::args().skip(1) {
+        let dlc = decoder.from_file(arg);
 
-        // print the result
         println!("DLC: {:?}", dlc);
     }
 }
+
 ```
 
 ## Thanks
+
 * [Bubblepoint](https://github.com/Bubblepoint) for creating and maintaing the crate.
 * [Robert Sch&uuml;tte](https://github.com/Roba1993) for doing a great refactoring.
 
 ## License
+
 Distributed under the [MIT License](LICENSE).
--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,114 @@
-//! ErrorChain mod to hold all errors.
+use std::error::Error as StdError;
+use std::fmt;
 
-error_chain!{
+#[derive(Debug)]
+pub enum ErrorKind {
+    Msg(String),
+    Io(std::io::Error),
+    Utf8(std::str::Utf8Error),
+    FromUtf8(std::string::FromUtf8Error),
+    Reqwest(reqwest::Error),
+    Regex(regex::Error),
+    Base64(base64::DecodeError),
+}
 
-    types {
-        Error, ErrorKind, ResultExt, Result;
-    }
-
-    foreign_links {
-        Fmt(::std::fmt::Error);
-        Io(::std::io::Error);
-        Utf8(::std::str::Utf8Error);
-        FromUtf8(::std::string::FromUtf8Error);
-        Reqwest(::reqwest::Error);
-        Regex(::regex::Error);
-        Base64(::base64::DecodeError);
+impl fmt::Display for ErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ErrorKind::Msg(s) => write!(f, "{}", s),
+            ErrorKind::Io(e) => write!(f, "IO error: {}", e),
+            ErrorKind::Utf8(e) => write!(f, "UTF-8 error: {}", e),
+            ErrorKind::FromUtf8(e) => write!(f, "FromUTF-8 error: {}", e),
+            ErrorKind::Reqwest(e) => write!(f, "Reqwest error: {}", e),
+            ErrorKind::Regex(e) => write!(f, "Regex error: {}", e),
+            ErrorKind::Base64(e) => write!(f, "Base64 error: {}", e),
+        }
     }
 }
+
+impl StdError for ErrorKind {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            ErrorKind::Msg(_) => None,
+            ErrorKind::Io(e) => Some(e),
+            ErrorKind::Utf8(e) => Some(e),
+            ErrorKind::FromUtf8(e) => Some(e),
+            ErrorKind::Reqwest(e) => Some(e),
+            ErrorKind::Regex(e) => Some(e),
+            ErrorKind::Base64(e) => Some(e),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Error {
+    kind: ErrorKind,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind) -> Self {
+        Error { kind }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.kind.fmt(f)
+    }
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        self.kind.source()
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::new(ErrorKind::Io(e))
+    }
+}
+
+impl From<&str> for Error {
+    fn from(e: &str) -> Self {
+        Error::new(ErrorKind::Msg(e.to_string()))
+    }
+}
+
+impl From<String> for Error {
+    fn from(e: String) -> Self {
+        Error::new(ErrorKind::Msg(e))
+    }
+}
+
+impl From<std::str::Utf8Error> for Error {
+    fn from(e: std::str::Utf8Error) -> Self {
+        Error::new(ErrorKind::Utf8(e))
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(e: std::string::FromUtf8Error) -> Self {
+        Error::new(ErrorKind::FromUtf8(e))
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Self {
+        Error::new(ErrorKind::Reqwest(e))
+    }
+}
+
+impl From<regex::Error> for Error {
+    fn from(e: regex::Error) -> Self {
+        Error::new(ErrorKind::Regex(e))
+    }
+}
+
+impl From<base64::DecodeError> for Error {
+    fn from(e: base64::DecodeError) -> Self {
+        Error::new(ErrorKind::Base64(e))
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
