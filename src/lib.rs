97fdebdc1c993e6b085a535855bfc139aa08462a
deimos <deimoshall.dev@proton.me>
2026-07-07 21:00:09 -0600

    docs: update example

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,14 @@
 //! ```rust
 //! extern crate dlc_decoder;
 //!
+//! use dlc_decoder::DlcDecoder;
+//! use std::env;
+//!
 //! fn main() {
 //!     let decoder = dlc_decoder::DlcDecoder::new();
 //!     for arg in std::env::args().skip(1) {
-//!         let dlc = decoder.from_file(arg);
-//!         println!("DLC: {:?}", dlc);
+//!         let dlc_package = decoder.from_file(arg);
+//!         dbg!(dlc_package.unwrap());
 //!     }
 //! }
 //! ```
